34c5b407bc735adafe7a7fa196b5dbc1c14f2c61
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2016-09-28 12:22:30 +0100

    Docking station releases bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,4 +5,9 @@ describe DockingStation do
     station1 = DockingStation.new
     expect(station1).to respond_to(:release_bike)
   end
+
+  it 'creates new bike when release_bike' do
+    station1 = DockingStation.new
+    expect(station1.release_bike).to be_instance_of(Bike)
+  end
 end
