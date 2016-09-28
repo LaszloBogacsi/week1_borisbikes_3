38757650f3c7e24ad9e19de9d2b879884ef85b3e
lcbeh <lichien2010@yahoo.co.uk>
2016-09-28 14:58:55 +0100

    Add bike docking feature

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,11 @@
 class DockingStation
+  attr_reader :bike
+
   def release_bike
- bike1 = Bike.new
+    bike1 = Bike.new
+  end
+
+  def dock(customer_bike)
+    @bike = customer_bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,4 +10,15 @@ describe DockingStation do
     station1 = DockingStation.new
     expect(station1.release_bike).to be_instance_of(Bike)
   end
+
+  it 'docks a bike' do
+    station1 = DockingStation.new
+    expect(station1).to respond_to(:dock).with(1).argument
+  end
+
+  it "reads docked bike" do
+    station1 = DockingStation.new
+    customer_bike = Bike.new
+    expect(station1.dock(customer_bike)).to eq(customer_bike)
+  end
 end
