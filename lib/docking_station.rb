bb8b0ba90ac345344d6bd6d28df69769f968e282
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2016-09-28 14:42:31 +0100

    require bike from dockingstation

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,14 @@
+require_relative 'bike'
+
 class DockingStation
+attr_reader :bike
+
   def release_bike
- bike1 = Bike.new
+ Bike.new
   end
+
+def dock(bike)
+  @bike = bike
+end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,4 +10,9 @@ describe DockingStation do
     station1 = DockingStation.new
     expect(station1.release_bike).to be_instance_of(Bike)
   end
+
+  it 'tests if bike is working' do
+    bike1 = subject.release_bike
+    expect(bike1).to be_working
+  end
 end
