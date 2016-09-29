faeb5b5ac20881cdfadfebc03ac8c1388850c101
LMCMLJ <mjcalvey2@gmail.com>
2016-09-29 22:27:46 +0100

    Saving current progress, will start again on bike fixing methods

--- lib/garage.rb
+++ lib/garage.rb
@@ -1,14 +1,31 @@
 class Garage
   attr_writer :fixed_bikes
+  attr_accessor :bikes_to_fix
+
+  def initialize
+    @fixed_bikes = []
+    @bikes_to_fix = []
+  end
+
   def bikes_from_van(incoming_bikes)
     @bikes_to_fix = incoming_bikes
   end
-
-  
 
   def bikes_to_van
     temp_fixed_bikes = @fixed_bikes
     @fixed_bikes = []
     temp_fixed_bikes
+  end
+
+  def fix_bike(bike_to_fix)
+    bike_to_fix.working = true
+  end
+
+  def process_bikes
+    @bikes_to_fix.map {|x|
+    fix_bike(x)
+    @fixed_bikes << x
+    }
+    @fixed_bikes
   end
 end
--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -1,10 +1,15 @@
 require 'garage'
 
 describe Garage do
+
+  let(:bike_broken) {double :bike_broken}
+
   before :each do
     @garage1 = Garage.new
     @fixed_bikes_in_test = ['bike1', 'bike2', 'bike3']
+    allow(bike_broken).to receive(:working?).and_return(false)
   end
+
   describe 'van transfer' do
     it 'receives bikes from van' do
       expect(@garage1.bikes_from_van(['bike1', 'bike2', 'bike3'])).to eq(@fixed_bikes_in_test)
@@ -17,7 +22,12 @@ describe Garage do
   end
 
   describe 'fixing bikes' do
-
+    it 'processes bike' do
+      bike = spy("bike")
+      @garage1.bikes_to_fix = [bike]
+      @garage1.process_bikes
+      expect(bike).to have_received(fix_bike)
+    end
   end
 
 
